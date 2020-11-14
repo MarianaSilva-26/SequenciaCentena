86a3079e08e775f16d648197739695ba8ba3ea89
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-11-14 16:16:48 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int numero = 1;
+
+        while (numero <= 100)
+        {
+            Console.Write(numero);
+            Console.Write(" ");
+            numero += 1;
+        }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("para sair Pressione Enter!");
+            Console.ReadKey();
+
+        }
+    }
+}
